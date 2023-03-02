5d5283c92e7f6424eae8ae8ce8cea14740e347dd
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-02 15:43:11 -0500

    implement light mode

--- src/components/Header/ThemeSlider.jsx
+++ src/components/Header/ThemeSlider.jsx
@@ -1,10 +1,98 @@
 import './Header.css';
+import { useRef, useState, useEffect } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi';
+import { STYLES } from '../../utils/constants';
 
 const ThemeSlider = () => {
+  const slider = useRef(null);
+  const [theme, setTheme] = useState('light');
+
+  const changeTheme = () => {
+    switch (theme) {
+      case 'dark':
+        setTheme('light');
+        break;
+      case 'light':
+        setTheme('dark');
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    switch (theme) {
+      case 'dark':
+        slider.current.style.right = '-3.8rem';
+        document.documentElement.style.setProperty(
+          '--color-bg',
+          STYLES.light.bg
+        );
+        document.documentElement.style.setProperty(
+          '--color-bg-alt',
+          STYLES.light.bgAlt
+        );
+        document.documentElement.style.setProperty(
+          '--color-primary',
+          STYLES.light.primary
+        );
+        document.documentElement.style.setProperty(
+          '--color-primary-alt',
+          STYLES.light.primaryAlt
+        );
+        document.documentElement.style.setProperty(
+          '--color-white',
+          STYLES.light.white
+        );
+        document.documentElement.style.setProperty(
+          '--color-light',
+          STYLES.light.light
+        );
+        document.body.style.backgroundImage =
+          'url(../src/assets/bg-texture-2.png)';
+        break;
+      case 'light':
+        slider.current.style.right = '-1.8rem';
+        document.documentElement.style.setProperty(
+          '--color-bg',
+          STYLES.dark.bg
+        );
+        document.documentElement.style.setProperty(
+          '--color-bg-alt',
+          STYLES.dark.bgAlt
+        );
+        document.documentElement.style.setProperty(
+          '--color-primary',
+          STYLES.dark.primary
+        );
+        document.documentElement.style.setProperty(
+          '--color-primary-alt',
+          STYLES.dark.primaryAlt
+        );
+        document.documentElement.style.setProperty(
+          '--color-white',
+          STYLES.dark.white
+        );
+        document.documentElement.style.setProperty(
+          '--color-light',
+          STYLES.dark.light
+        );
+        document.body.style.backgroundImage = '';
+        break;
+      default:
+        break;
+    }
+  }, [theme]);
+
   return (
-    <div className='header__theme-slider'>
-      <i className='header__theme-slider__selector' />
+    <div
+      className='header__theme-slider'
+      onClick={changeTheme}
+    >
+      <i
+        className='header__theme-slider__selector'
+        ref={slider}
+      />
       <div className='header__theme-slider__toggle'>
         <FiSun />
         <FiMoon />
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const STYLES = {
+  dark: {
+    bg: 'rgb(47, 62, 70)',
+    bgAlt: 'rgb(53, 79, 82)',
+    primary: 'rgb(132, 169, 140)',
+    primaryAlt: 'rgba(82, 121, 111, 0.4)',
+    white: 'rgb(218, 218, 218)',
+    light: 'rgba(202, 210, 197, 0.6)'
+  },
+  light: {
+    bg: 'rgb(255, 255, 240)',
+    bgAlt: 'rgba(202, 210, 197, 0.6)',
+    primary: 'rgba(82, 121, 111, 1)',
+    primaryAlt: 'rgb(132, 169, 140)',
+    white: 'rgb(47, 62, 70)',
+    light: 'rgb(53, 79, 82)'
+  }
+};
