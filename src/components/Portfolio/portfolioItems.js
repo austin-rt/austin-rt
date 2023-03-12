d804e2de38427cb6b9479e2c7f7febe4d476055a
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2023-03-12 13:09:41 -0400

    update block invaders links and stack

--- src/components/Portfolio/portfolioItems.js
+++ src/components/Portfolio/portfolioItems.js
@@ -15,8 +15,9 @@ import {
   SiNetlify,
   SiVercel,
   SiRedux,
+  SiTypescript
 } from 'react-icons/si';
-import { FaNodeJs } from 'react-icons/fa';
+import { FaNodeJs, FaFly } from 'react-icons/fa';
 import { TbBrandTailwind } from 'react-icons/tb';
 
 export const portfolioItems = [
@@ -28,7 +29,13 @@ export const portfolioItems = [
     demo: 'https://thunderbird-68145.web.app/',
     oneline:
       'brtr functions as a simulated buy / sell platform for any type of item. You can tell how cool and hip and 2010s we are by the lack of vowels in our name. Users can browse as a guest or create an account for the full experience. With an account users will be able to list and sell thier items and buy other users items. A post MVP version sees reviews for both users and items, credit card processing, user and product reviews, and a barter system to trade items instead of using money.',
-    stack: [<SiFirebase />, <DiReact />, <SiRedux />, <FaNodeJs />, <TbBrandTailwind />],
+    stack: [
+      <SiFirebase />,
+      <DiReact />,
+      <SiRedux />,
+      <FaNodeJs />,
+      <TbBrandTailwind />
+    ]
   },
   {
     id: 1,
@@ -44,8 +51,8 @@ export const portfolioItems = [
       <DiReact />,
       <FaNodeJs />,
       <SiVercel />,
-      <SiRailway />,
-    ],
+      <SiRailway />
+    ]
   },
   {
     id: 2,
@@ -63,8 +70,8 @@ export const portfolioItems = [
       <FaNodeJs />,
       <TbBrandTailwind />,
       <SiNetlify />,
-      <SiRailway />,
-    ],
+      <SiRailway />
+    ]
   },
   {
     id: 3,
@@ -80,18 +87,24 @@ export const portfolioItems = [
       <DiReact />,
       <FaNodeJs />,
       <SiVercel />,
-      <SiRailway />,
-    ],
+      <SiRailway />
+    ]
   },
   {
     id: 4,
     image: blockInvaders,
     title: 'Block Invaders',
     github: 'https://github.com/austin-rt/blockinvaders',
-    demo: 'https://austinrt.io/blockinvaders/',
+    demo: 'https://block-invaders.fly.dev/',
     oneline:
       "Block Invaders is a multi-level, in-browser version of the classic arcade game Space Invaders. The user controls a block that can shoot and move left or right while the enemy descends toward the player. As the level increments, the speed of the invaders' descension increases.",
-    stack: [<DiHtml5 />, <DiCss3 />, <DiJavascript />],
+    stack: [
+      <DiHtml5 />,
+      <DiCss3 />,
+      <DiJavascript />,
+      <SiTypescript />,
+      <FaFly />
+    ]
   },
   {
     id: 5,
@@ -101,6 +114,12 @@ export const portfolioItems = [
     demo: 'https://crapple-maps.herokuapp.com/',
     oneline:
       'Crapple Maps is a simple, crowdsourced, full-stack, public restroom locator. This was built using the MERN stack. When a user is in need of a public restroom, they will be able to access the database in list form. If a listing is perceived as inaccurate, users will be able to update or delete the listing.',
-    stack: [<SiMongodb />, <SiExpress />, <DiReact />, <FaNodeJs />, <SiHeroku />],
-  },
+    stack: [
+      <SiMongodb />,
+      <SiExpress />,
+      <DiReact />,
+      <FaNodeJs />,
+      <SiHeroku />
+    ]
+  }
 ];
