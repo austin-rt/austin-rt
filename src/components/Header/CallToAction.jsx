385265b4077b245924305fc34e15b23728e8ee77
Austin Ryan Taylor <97855093+austin-rt@users.noreply.github.com>
2024-11-05 10:40:14 -0500

    add view resume

--- src/components/Header/CallToAction.jsx
+++ src/components/Header/CallToAction.jsx
@@ -1,23 +1,29 @@
-import { useContext } from 'react';
-import { RefContext } from '../../context/RefContext';
-import Resume from '../../assets/Austin_Taylor-ATS_Resume-2023_02.pdf';
+import { useContext } from "react";
+import { RefContext } from "../../context/RefContext";
+import Resume from "../../assets/Austin_Taylor-ATS_Resume-2024_11_05";
 
 const CallToAction = () => {
   const { contact, scrollTo } = useContext(RefContext);
   return (
-    <div className='call-to-action'>
+    <div className="call-to-action">
       <a
-        href={Resume}
-        download
-        className='btn'
+        href="https://docs.google.com/document/d/1-zDNzVHm6cU_RbuZKNaCGMaCCzErdtVP5BcP9RhUuWA/preview"
+        target="_blank"
+        className="btn btn-primary"
+        role="button"
+        rel="noreferrer"
       >
+        View Resume
+      </a>
+      <a href={Resume} download className="btn" role="button">
         Download Resume
       </a>
       <div
         onClick={() => {
           scrollTo(contact);
         }}
-        className='btn btn-primary'
+        className="btn btn-primary"
+        role="button"
       >
         Let's Chat
       </div>
