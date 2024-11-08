22adb730a70faba5d6518d5e64a6d2bca508780c
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2024-11-08 13:39:23 -0500

    dynamiclly link resume download. rm view resume

--- src/components/Header/CallToAction.jsx
+++ src/components/Header/CallToAction.jsx
@@ -1,21 +1,12 @@
 import { useContext } from "react";
 import { RefContext } from "../../context/RefContext";
-import Resume from "../../assets/Austin_Taylor-ATS_Resume-2024_11_08.pdf";
+import { API } from "../../utils/constants";
 
 const CallToAction = () => {
   const { contact, scrollTo } = useContext(RefContext);
   return (
     <div className="call-to-action">
-      <a
-        href="https://docs.google.com/document/d/1-zDNzVHm6cU_RbuZKNaCGMaCCzErdtVP5BcP9RhUuWA/preview"
-        target="_blank"
-        className="btn btn-primary"
-        role="button"
-        rel="noreferrer"
-      >
-        View Resume
-      </a>
-      <a href={Resume} download className="btn" role="button">
+      <a href={API.resumeExport} download className="btn" role="button">
         Download Resume
       </a>
       <div
